a02e3d4b51ebc7373c2efffa0bb52c2dd7d6561b
Mai Vang <vmaineng@gmail.com>
2022-06-04 20:49:05 -0700

    commit tree 49e90e9b4ac796934ba2bd5b05213e5c6cc32299

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(number)
+   return number/2
+    end
+
+   if number.class != "integer"
+    return nil;
+end
